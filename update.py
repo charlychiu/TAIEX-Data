9679cdd7cee4ed123860d7919b4eb5e8f866dc87
charlychiu <charlychiu@gmail.com>
2021-01-28 00:32:46 +0800

    Update update.py

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 my_headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15'}
-filename = datetime.today().strftime('%Y%m%d')
+filename = datetime.today().strftime('%Y%m01')
 y = datetime.today().strftime('%Y')
 if not os.path.exists('history/' + str(y)):
     os.makedirs('history/' + str(y))
